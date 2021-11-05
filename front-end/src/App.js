aca001b8ec59cb2c9172fb2751690e4debe17a1b
aleyna <clkkaley@gmail.com>
2021-11-05 12:32:10 +0300

    react-bootstrap-setup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,12 @@ import {Container} from 'react-bootstrap'
 import Header from './component/Header'
 import Footer from './component/Footer'
 
+
 const  App = ()=> {
   return (
     <>
     <Header />
-      <main className='py-2'> //**navbar ile arasına boşluk */
+      <main className='py-2'> 
         <Container>
             <h1>hello</h1>
         </Container>
