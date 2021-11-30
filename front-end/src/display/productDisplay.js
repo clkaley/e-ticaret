bf72f570fdb7161ee483242284c7dec1a988e009
aleyna <clkkaley@gmail.com>
2021-12-01 00:57:44 +0300

    product details

--- front-end/src/display/productDisplay.js
+++ front-end/src/display/productDisplay.js
@@ -9,27 +9,82 @@ function ProductDisplay() {
   const product = products.find((p) => p._id === match.id);
  
   return <div>
-        <Link className='btn btn-light my-3' to=''>
-          Go Back
-        </Link>
-        <Row>
+       
+        <Row className='pt-5'>
+          </Row>
+        <Row className='mr-2'>
           
-            <Col md={6} className="product-detail" >
-              <Image  src={product.image} alt={product.name} />
+            <Col  md={5} >
+              <Image  src={product.image} alt={product.name} fluid />
             </Col>
          
-            <Col md={3} >
+         <Col sm={1}></Col>
+            <Col md={4}  className=' mx-5'>
               <ListGroup variant="flush">
                   <ListGroupItem>
                     <h2>{product.name}</h2>
                   </ListGroupItem>
               </ListGroup>
               <ListGroupItem>
-                <Rating value={product.rating} text={`${product.total_comments} comments`} />
+                <Rating  value={product.rating} text={`${product.total_comments} comments`} />
               </ListGroupItem>
+             
+
+              <ListGroupItem>
+                 Description:{product.description}
+              </ListGroupItem>
+              <Card>
+                  <ListGroup>
+                    <ListGroupItem>
+                      <Row>
+                      <Col>
+                       Price: 
+                        </Col>
+                        <Col>
+                        <strong>{product.price}</strong>
+                        </Col>
+                      </Row>
+                    </ListGroupItem>
+                  </ListGroup>
+
+                  <ListGroup>
+                    <ListGroupItem>
+                      <Row>
+                      <Col>
+                       Stock: 
+                        </Col>
+                        <Col>
+                        {product.stock > 0 ? 'In Stock': 'OUT OF STOCK'}
+                        </Col>
+                      </Row>
+                    </ListGroupItem>
+                  </ListGroup>
+                  <ListGroupItem className='mx-auto'>
+                    <Button className='btn-block btn-danger ' type='button' disabled={product.stock===0}>
+                      Add To Shopping Cart
+                    </Button>
+                  </ListGroupItem>
+
+                </Card>
+              </Col>
+              <Col md={3}>
+               
+             
             </Col>
         </Row>
     </div>
 }
  
 export default ProductDisplay
+
+/*<ListGroupItem>
+                 {product.price}
+              </ListGroupItem>
+*/
+
+
+/**
+<Link className='btn btn-primary my-3 ms-auto"' to='./'>
+          Go Back
+        </Link>
+ */
--- front-end/src/products.js
+++ front-end/src/products.js
@@ -15,10 +15,11 @@ const products = [
     {
         _id:'1',
         name: 'Instax Mini 9 ',
-        image:'/image-products/pexels-timothy-paule-ii-1433191',
-        description:'lorem ipsum dolor',
+        image:'/image-products/instax.png',
+        description:'The high-performance flash automatically calculates surrounding brightness and adjusts shutter speed accordingly — no need for any special settings.',
         brand:'FujiFilm',
         category:'Electronics',
+        stock:15,
         price:'$60',
         total_comments:2,
         rating:5,
@@ -29,24 +30,26 @@ const products = [
         _id:'2',
         name: 'Instax Mini 8',
         image:'/image-products/instax-y.jpg',
-        description:'lorem ipsum dolor',
+        description:'The light of the flash does not reach the background, resulting in a dark-looking photo.',
         brand:'FujiFilm',
         category:'Electronics',
         price:'$80',
         total_comments:1,
         rating: 4,
+        stock:25,
     },
 
     {
         _id:'3',
         name: 'Instax Mini 11',
         image:'/image-products/instax-w.jpg',
-        description:'lorem ipsum dolor',
+        description:'mini 11 captures bright backgrounds and bright subjects even in dark scenes.',
         brand:'FujiFilm',
         category:'Electronics',
         price:'$95',
         total_comments:5,
         rating: 2.5,
+        stock:0,
     },
 
     {
@@ -59,6 +62,7 @@ const products = [
         price:'$120',
         total_comments:12,
         rating: 3.5,
+        stock:65,
     },
 ]
 
