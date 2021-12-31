8c9a65743dff20d0ad02554f5b98d7db229e15fe
aleyna <clkkaley@gmail.com>
2021-12-31 11:40:51 +0300

    products-by-order

--- back-end/server.js
+++ back-end/server.js
@@ -4,13 +4,24 @@ const products=require('./data/products')
 
 const app=express()
 
+//çalıştığını gösteren
 app.get('/',(reg,res) => {
     res.send("API is running...")
 })
 
+//ürünleri gösteren
 app.get('/api/products',(reg,res) => {
   res.json(products)
 })
+
+//ürünleri id ile gösteren
+app.get('/api/products/:id',(reg,res) => {
+  const product =products.find((p) => p._id===req.params.id)
+  res.json(product)
+})
+
+
+
 
 
 app.listen(5000,console.log("Server Running On Port 5000"))
