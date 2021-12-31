81afcab16a2c8cb8814a3ebc4da95fd2163c4afd
aleyna <clkkaley@gmail.com>
2021-12-31 11:44:17 +0300

    product-order

--- back-end/server.js
+++ back-end/server.js
@@ -5,18 +5,18 @@ const products=require('./data/products')
 const app=express()
 
 //çalıştığını gösteren
-app.get('/',(reg,res) => {
+app.get('/',(req,res) => {
     res.send("API is running...")
 })
 
 //ürünleri gösteren
-app.get('/api/products',(reg,res) => {
+app.get('/api/products',(req,res) => {
   res.json(products)
 })
 
 //ürünleri id ile gösteren
-app.get('/api/products/:id',(reg,res) => {
-  const product =products.find((p) => p._id===req.params.id)
+app.get('/api/products/:id',(req,res) => {
+  const product =products.find((p) => p._id === req.params.id)
   res.json(product)
 })
 
