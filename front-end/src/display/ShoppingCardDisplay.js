74b8ce46e90dfc3d6ae1fca39dc4855cbb2822de
aleyna <clkkaley@gmail.com>
2022-02-20 14:44:36 +0300

    card-function

--- front-end/src/display/ShoppingCardDisplay.js
+++ front-end/src/display/ShoppingCardDisplay.js
@@ -1,7 +1,7 @@
 import React,{ useEffect } from 'react'
-import{Link} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 import {useDispatch, useSelector }from   'react-redux';
-import {Row,Col,ListGroup,Image,Form,Button,Card} from 'react-bootstrap'
+import {Row,Col,ListGroup,Image,Form,Button,Card, ListGroupItem} from 'react-bootstrap'
 import { addToCard } from '../action/cardAction'
 import Message from '../component/Message';
 //import { useParams } from 'react-router-dom';
@@ -26,6 +26,11 @@ const ShoppingCardDisplay = (location) => {
       dispatch(addToCard(productId,qty))
     }
   }, [dispatch,productId,qty])
+
+  const removeFromCardHandler=(id) =>{
+    console.log('remove');
+  }
+
  
  /* const { id } = useParams();
   const productId=useParams();*/
@@ -67,7 +72,61 @@ const ShoppingCardDisplay = (location) => {
   }, [dispatch,productId,qty])*/
 
   return (
-    <div>ShoppingCardDisplay</div>
+    
+      <Row>
+        <Col md={8}>
+        <h1 >Shopping Card</h1>
+        {cardItems.length ===0 ? <Message className="">Your Shopping Cart is empty  <Link className='btn btn-info  btn-sm ms-auto  "' to='/'>
+                        Go Back
+                  </Link>    </Message> : (
+          <ListGroup variant='flush'>
+            {cardItems.map(item=>(
+              <ListGroupItem key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded></Image>
+                  </Col>
+                  <Col md={2}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>
+                    {item.price}
+                  </Col>
+                  <Col md={2}>
+                  <Form.Control as="select" value={item.qty} onChange={(e)=>dispatch( addToCard(item.product, Number(e.target.value)))}>
+                             {
+                              [...Array(item.stock).keys()].map((x) =>(
+                                <option key={ x + 1 } value={ x + 1 }>{ x + 1 }</option>
+                              ))}
+                              
+                            </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button type="button" variant='light' onClick={()=> removeFromCardHandler(item.product)}>
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+
+              </ListGroupItem>
+            ))}
+          </ListGroup>
+          
+        )}
+       </Col>
+          <Col md={4}>
+              <Card>
+                <ListGroup variant='flush'>
+                  <ListGroupItem>
+                    <h2>Total ({cardItems.reduce((acc,item)=> (acc + item.qty),0)}) Items</h2>
+                    ${cardItems.reduce((acc,item)=>acc+item.qty * item.price,0).toFixed(2)}
+                  </ListGroupItem>
+                </ListGroup>
+              </Card>
+          </Col>
+          
+      </Row>
+   
   )
 }
 
